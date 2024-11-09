f0496793e79e27311aaccd116032a9860b027de8
Ali <m.ali.rezaeiii09@gmail.com>
2024-11-09 21:49:34 +0330

    Initial Commit

---
+++ NetworkingAssignment/NetworkingAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingAssignmentApp.swift
+//  NetworkingAssignment
+//
+//  Created by Ali on 11/9/24.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
