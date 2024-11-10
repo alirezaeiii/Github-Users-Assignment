f40e99251ca08bdca892d96a3b431414b7889006
Ali <m.ali.rezaeiii09@gmail.com>
2024-11-10 22:57:12 +0330

    Display following list as well

---
+++ NetworkingAssignment/UserWrapper.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  NetworkingAssignment
+//
+//  Created by Ali on 11/10/24.
+//
+
+import Foundation
+
+struct UserWrapper {
+    let section: String
+    let users: [GithubUser]
+}
