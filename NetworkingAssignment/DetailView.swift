71713834e30de87c8debc051871d0548c486f3d1
Ali <m.ali.rezaeiii09@gmail.com>
2024-11-10 20:33:01 +0330

    Add Detail View

---
+++ NetworkingAssignment/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  NetworkingAssignment
+//
+//  Created by Ali on 11/10/24.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
