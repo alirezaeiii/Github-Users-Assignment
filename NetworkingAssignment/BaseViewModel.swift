ffa298792611d9d2be864d863264359d5756db67
Ali <m.ali.rezaeiii09@gmail.com>
2024-11-12 01:25:03 +0330

    Add Base ViewModel

--- NetworkingAssignment/BaseViewModel.swift
+++ NetworkingAssignment/BaseViewModel.swift
@@ -1,49 +1,8 @@
 //
-//  GithubViewModel.swift
+//  BaseViewModel.swift
 //  NetworkingAssignment
 //
-//  Created by Ali on 11/10/24.
+//  Created by Ali on 11/12/24.
 //
 
 import Foundation
-
-class BaseViewModel<T>: ObservableObject {
-    typealias GHError = APIService.GHError
-    
-    @Published var result = Resource.loading
-    
-    let apiService = APIService()
-    
-    init() {
-        refresh()
-    }
-    
-    func getSuccessResult() async throws -> T? {
-        return nil
-    }
-    
-    func refresh() {
-        result = Resource.loading
-        Task { @MainActor in
-            do {
-                if let successResult = try await getSuccessResult() {
-                    result = Resource.success(successResult)
-                }
-            } catch GHError.invalidURL {
-                result = Resource.error("Invalid URL")
-            } catch GHError.invalidResponse {
-                result = Resource.error("Invalid response")
-            } catch GHError.invalidData {
-                result = Resource.error("Invalid data")
-            } catch {
-                result = Resource.error("Unexpected error")
-            }
-        }
-    }
-    
-    enum Resource {
-        case loading
-        case success(T)
-        case error(String)
-    }
-}
