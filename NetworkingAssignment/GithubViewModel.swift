0fc6bd12126b05ce8149da9dac10696292b6b45b
Ali <m.ali.rezaeiii09@gmail.com>
2024-11-12 01:09:08 +0330

    Add Base ViewModel

--- NetworkingAssignment/GithubViewModel.swift
+++ NetworkingAssignment/GithubViewModel.swift
@@ -1,52 +1,23 @@
 //
-//  GithubViewModel.swift
+//  SubViewModel.swift
 //  NetworkingAssignment
 //
-//  Created by Ali on 11/10/24.
+//  Created by Ali on 11/11/24.
 //
 
 import Foundation
 
-class GithubViewModel: ObservableObject {
-    typealias GHError = APIService.GHError
+class GithubViewModel : BaseViewModel<[UserWrapper]> {
     
-    @Published var result = Resource.loading
-    
-    private let apiService = APIService()
-    
-    init() {
-        refresh()
-    }
-    
-    func refresh() {
-        result = Resource.loading
-        Task { @MainActor in
-            async let following = apiService.getUsers(endPoint: Constants.followingEndPoint)
-            async let followers = apiService.getUsers(endPoint: Constants.followersEndPoint)
-            do {
-                let (fetchedFollowing, fetchedFollowers) = try await (following, followers)
-                result = Resource.success(UserWrapper.createUsers(following: fetchedFollowing, followers: fetchedFollowers))
-            } catch GHError.invalidURL {
-                result = Resource.error("Invalid URL")
-            } catch GHError.invalidResponse {
-                result = Resource.error("Invalid response")
-            } catch GHError.invalidData {
-                result = Resource.error("Invalid data")
-            } catch {
-                result = Resource.error("Unexpected error")
-            }
-        }
+    override func getSuccessResult() async throws -> [UserWrapper] {
+        async let following = apiService.getUsers(endPoint: Constants.followingEndPoint)
+        async let followers = apiService.getUsers(endPoint: Constants.followersEndPoint)
+        return try await UserWrapper.createUsers(following: following, followers: followers)
     }
     
     private struct Constants {
         private static let endPoint = "https://api.github.com/users/alirezaeiii/"
         static let followingEndPoint = endPoint + "following"
         static let followersEndPoint = endPoint + "followers"
-    }
-    
-    enum Resource {
-        case loading
-        case success([UserWrapper])
-        case error(String)
     }
 }
