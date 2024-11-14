4ddd7fbd93ac8fe707bfedaab361da8343285799
Ali <m.ali.rezaeiii09@gmail.com>
2024-11-15 00:44:40 +0330

    Create UserItemView

---
+++ NetworkingAssignment/view/UserItemView.swift
@@ -0,0 +1,18 @@
+//
+//  UserItemView.swift
+//  NetworkingAssignment
+//
+//  Created by Ali on 11/15/24.
+//
+
+import SwiftUI
+
+struct UserItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserItemView()
+}
