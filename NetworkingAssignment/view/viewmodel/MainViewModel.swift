6421ca360079583bd2d0daec932b319b8ac1f09b
Ali <m.ali.rezaeiii09@gmail.com>
2024-11-27 18:44:43 +0330

    Add Detail Screen with bio

--- NetworkingAssignment/view/viewmodel/MainViewModel.swift
+++ NetworkingAssignment/view/viewmodel/MainViewModel.swift
@@ -10,9 +10,6 @@ import OSLog
 
 class GithubViewModel : ObservableObject {
     
-    /// A logger for debugging.
-        fileprivate let logger = Logger(subsystem: "com.sample.NetworkingAssignment", category: "parsing")
-    
     @Published var viewState: ViewState = .loading
     @Published var users: [UserWrapper] = []
     
@@ -26,13 +23,10 @@ class GithubViewModel : ObservableObject {
             async let following = GithubUser.getUsers(endPoint: Constants.followingEndPoint)
             async let followers = GithubUser.getUsers(endPoint: Constants.followersEndPoint)
             do {
-                logger.debug("Refreshing the data ...")
                 let (fetchedFollowing, fetchedFollowers) = try await (following, followers)
                 users = UserWrapper.createUsers(following: fetchedFollowing, followers: fetchedFollowers)
-                logger.debug("Refresh complete.")
                 self.viewState = .completed
             } catch {
-                logger.error("\(error.localizedDescription)")
                 viewState = .failure(error: error)
             }
         }
